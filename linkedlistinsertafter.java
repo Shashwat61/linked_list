e30c8ef6f59c3912b3cb9c8329d69f184040e714
Shashwat61 <68372070+Shashwat61@users.noreply.github.com>
2020-07-31 01:33:06 +0530

    Update linkedlistinsertafter.java

--- linkedlistinsertafter.java
+++ linkedlistinsertafter.java
@@ -1,23 +1,23 @@
 import java.util.*;
-class linkedlist_insertafter
+class linkedlistinsertafter
 {
     class Node
     {
         Node next;
-        int data;
-        Node(int d)
+        int dat;
+        Node(int c)
         {
-            data=d;
+            dat=c;
             next=null;
         }
     }
     Node head;
-    public void insert(int d)
+    public void insert(int c)
     {
-         Node ele=new Node(d);
+         Node element=new Node(c);
          if(head==null)
          {
-           head=ele;
+           head=element;
          }
          else{
             Node last =head;
@@ -25,54 +25,57 @@ class linkedlist_insertafter
             {
                 last=last.next;
             }
-            last.next=ele;
+            last.next=element;
          }
     }
-    public void disp()
+    public void display()
    {
-      Node curr =head;
-      if(curr==null)
+      Node current =head;
+      if(current==null)
       {
           System.out.print("empty");
       }
-      else{System.out.println("elements are:");
-          while(curr !=null)
+      else
+      {
+          System.out.println("elements are:");
+          while(current !=null)
           {
-              System.out.print(curr.data+ " ");
-              curr=curr.next;
+              System.out.print(current.data+ " ");
+              current=current.next;
           }
           System.out.println();
       }
    }
-   public void insertafter(int e,int pos)
-   {  Node el=new Node(e);
-       Node curr=head;
+   public void insertafter(int l,int position)
+   {  Node element=new Node(l);
+       Node current=head;
        int i=1;
-       while(i<pos)
+       while(i<position)
        {
-           curr=curr.next;
+           current=current.next;
            i++;
        }
-      el.next=curr.next;
-      curr.next=el;
+      element.next=curr.next;
+      currrent.next=element;
    }
     public static void main(String args[])
     {
+        
         Scanner sc =new Scanner(System.in);
-        int n=sc.nextInt();
-        linkedlist_insertafter ob= new linkedlist_insertafter();
-        for(int i=0;i<n;i++)
+        int m=sc.nextInt();
+        linkedlistinsertafter ob= new linkedlistinsertafter();
+        for(int i=0;i<m;i++)
         {
-            int el=sc.nextInt();
-            ob.insert(el);
+            int element=sc.nextInt();
+            ob.insert(element);
         }
-        ob.disp();
+        ob.display();
         System.out.println("enter element");
-        int e=sc.nextInt();
+        int l=sc.nextInt();
         System.out.println("enter postion");
-        int pos=sc.nextInt();
-        ob.insertafter(e,pos);
-        ob.disp();
+        int position=sc.nextInt();
+        ob.insertafter(l,position);
+        ob.display();
         sc.close();
     }
 }
